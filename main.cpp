08896f634d49fc6c28a52d7a8d4aaabcd7571523
Adam Di Cioccio <adamdicioccio@gmail.com>
2022-01-24 15:39:21 -0500

    Demo#2

--- main.cpp
+++ main.cpp
@@ -35,9 +35,9 @@ class Vehicle {
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    cout << "Demo #1\n";
+    cout << "Demo #2\n";
     
-    Vehicle v1;
+    Vehicle v1(4);
     
     cout << "Vehicle has been created\n";
     return 0;
