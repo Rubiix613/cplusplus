abd8eb00ce3973a0104b360f1242d48de01e5566
Adam Di Cioccio <adamdicioccio@gmail.com>
2022-01-24 15:06:06 -0500

    Initial Commit with print message

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Lab01
+//
+//  Created by Adam Di Cioccio on 2022-01-24.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "This is the initial commit!\n";
+    return 0;
+}
