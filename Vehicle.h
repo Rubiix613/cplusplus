8580f692584b44f425c75e2bf39e8c7369d54770
Adam Di Cioccio <adamdicioccio@gmail.com>
2022-02-06 20:00:32 -0500

    Finished Week4

--- Vehicle.h
+++ Vehicle.h
@@ -7,6 +7,37 @@
 
 #pragma once
 
+#include <stdio.h>
+#include <iostream>
+
+class Vehicle {
+private:
+    int _w, _d;
+
+public:
+    Vehicle();
+
+    Vehicle(int w);
+
+    Vehicle(int w, int d);
+    
+    ~Vehicle();
+    
+    void setWheels(int w);
+    int getWheels();
+    
+    void setDoors(int d);
+    int getDoors();
+    
+    void printVehicle();
+    
+    Vehicle (Vehicle &);
+    
+    Vehicle (Vehicle *);
+
+};
+
+
 #ifndef Vehicle_h
 #define Vehicle_h
 
--- main.cpp
+++ main.cpp
@@ -5,98 +5,31 @@
 //  Created by Adam Di Cioccio on 2022-01-24.
 //
 
-#include <iostream>
 #include "Vehicle.h"
 
 using namespace std;
 
-class Vehicle {
-    private:
-    int _w, _d;
-    
-    public:
-    Vehicle(int w, int d) {
-        _w = w;
-        _d = d;
-        cout << "In constructor with 2 parameters" << endl;
-    }
-    Vehicle(int w) : Vehicle(0, 0) {
-        cout << "In constructor with 1 parameters, w = " << w << endl;
-    }
-    Vehicle() : Vehicle(0) {
-        cout << "In constructor with 0 parameters" << endl;
-    }
-    ~Vehicle() {
-        cout << "In destructor" << endl;
-    }
-    
-    
-};
-
-Vehicle veh1;
-Vehicle veh2(4);
-Vehicle veh3(4, 2);
-
-int getInt(string type) {
-    
-    bool valid = false;
-    char input[128];
-    int i = 0;
-    string quit = "q";
-
-    while(!valid) {
-        cout << "Enter number of " << type << "\n>";
-        cin >> input;
-        
-        string temp = input;
-        
-        if (input == quit) {
-            cout << "Quitting program..." << endl;
-            exit(0);
-        }
-        
-        i = atoi(temp.c_str());
-        
-        if ( i == 0 ) {
-            cout << "Invalid input! (Not an integer)" << endl;
-        } else if (i < 0) {
-            cout << "Invalid input! (Not a positive value)" << endl;
-        } else {
-            valid = true;
-        }
-        
-    }
-    cin.clear();
-    return i;
+void CreateVehicle(Vehicle &v, int w = 10, int d = 8) {
+    v.setWheels(w);
+    v.setDoors(d);
 }
 
-
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    cout << "Corrected Demo\n";
+    Vehicle original ; //empty constructor no ( )
+    Vehicle copy(original); // copy constructor by reference
+    Vehicle secondCopy( &original ); //copy constructor by pointer
+    copy.printVehicle ();
     
-    bool programRunning = true;
-    
-    while (programRunning) {
-        int door, wheel;
-        
-        Vehicle *pVehicle;
-        
-        cout << "- CREATE NEW VEHICLE | [q] to quit -" << endl;
-        
-        door =  getInt("doors");
-        
-        wheel = getInt("wheels");
-        
-        cout << "You entered: " << door << ", " << wheel << "\n" << endl;
-        
-        pVehicle = new Vehicle(door, wheel);
-        
-        delete(pVehicle);
-        
-        cout << "Vehicle has been created\n\n";
-    }
+    CreateVehicle(copy, 3); //wheels is 3, everything else is default
+    copy.printVehicle();
+    CreateVehicle(copy, 3, 6); //wheels is 3, doors is 6
+    copy.printVehicle ();
+    copy = secondCopy;
+    copy.printVehicle(); // copy is same as second copy
     return 0;
+    
+    
+    
 }
 
 
